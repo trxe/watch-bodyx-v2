26e73554f195ce291dbe2816569a47d33b0608df
trxe <peixianwong@gmail.com>
2022-03-25 00:50:22 +0800

    final update to socket url

--- client/config/default.ts
+++ client/config/default.ts
@@ -1,1 +1,3 @@
-export const  SOCKET_URL = process.env.SOCKET_URL || 'http://localhost:4000';
+export const  SOCKET_URL = process.env.SOCKET_URL || 'http://128.199.189.188/';
+
+// 'http://localhost:4000'
