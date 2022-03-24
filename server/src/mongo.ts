8ea561549beeb9835443c59ebf488953a118c9d1
trxe <peixianwong@gmail.com>
2022-03-24 22:36:35 +0800

    update docker files

--- server/src/mongo.ts
+++ server/src/mongo.ts
@@ -6,9 +6,11 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const mongoInit = () => {
-    mongoose.connect(process.env.DB_URL)
-        .then(() => {
-            Logger.info(`MongoDB connected at ${process.env.DB_URL}`);
-        })
-        .catch(err => Logger.error(err))
+    mongoose.connect(process.env.DB_URL, {
+        dbName: process.env.DB_NAME,
+    })
+    .then(() => {
+        Logger.info(`MongoDB connected at ${process.env.DB_URL}`);
+    })
+    .catch(err => Logger.error(err))
 }
