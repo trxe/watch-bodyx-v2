dc68918943da20f567e6f3054f15ea547bc38930
trxe <peixianwong@gmail.com>
2022-05-14 22:25:35 +0800

    attendees layout

--- client/containers/Attendees.tsx
+++ client/containers/Attendees.tsx
@@ -1,12 +1,18 @@
 import { useSockets } from "../context/socket.context";
-import { HiOutlineTicket } from "react-icons/hi"
+import { HiOutlineTicket, HiStatusOffline } from "react-icons/hi"
 import dashboard from '../styles/Dashboard.module.css'
 import styles from '../styles/Attendees.module.css'
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { classList } from "../utils/utils";
+import { BiRefresh } from "react-icons/bi";
+import { RiArrowDownSFill, RiArrowDownSLine } from "react-icons/ri";
 
 const Attendee = ({name, ticket}) => {
-    return <div key={ticket} className={styles.attendee}>
-        <p>{name}</p>
-        <p>{ticket}</p>
+    return <div className={classList(styles.attendee, styles.row)}>
+        <div className={styles.icon}><HiStatusOffline/></div>
+        <div className={styles.ticket}>{ticket}</div>
+        <div className={styles.name}>{name}</div>
+        <button className={styles.icon}><BsThreeDotsVertical/></button>
     </div>;
 }
 
@@ -17,8 +23,18 @@ const AttendeesContainer = (props) => {
         <div className={dashboard.containerHeader}>
             <HiOutlineTicket />
             <div className={dashboard.containerTitle}>TICKET HOLDERS</div>
+            <div className={classList(dashboard.refresh)}><BiRefresh/></div>
         </div>
         <div className={dashboard.containerContent}>
+            <div className={classList(styles.attendee, styles.header)}>
+                <div className={styles.icon}></div>
+                <div className={styles.ticket}>Ticket 
+                    <button><RiArrowDownSFill/></button>
+                </div>
+                <div className={styles.name}>Name
+                    <button><RiArrowDownSLine/></button>
+                </div>
+            </div>
             {show.attendees && 
                 Array.from(show.attendees.values())
                     .map(attendee => 
